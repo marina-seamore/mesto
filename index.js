f2b4fe8762ea2663e56fcafc660e9212250e47bd
Marina Abovyan <marrina.abovyan@gmail.com>
2020-11-14 12:05:23 +0300

    commit!

---
+++ index.js
@@ -0,0 +1,27 @@
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
+let popupClose = document.querySelector('.popup__close-button');
+let form = document.querySelector('.popup__content');
+let nameField = document.querySelector('.popup__field_name');
+let descriptionField = document.querySelector('.popup__field_description')
+let name = document.querySelector('.profile__name');
+let description = document.querySelector('.profile__description');
+
+
+function showPopup() {
+  popup.classList.add('popup_opened');
+}
+editButton.addEventListener('click', showPopup);
+
+function closePopup() {
+    popup.classList.remove('popup_opened');
+}
+popupClose.addEventListener('click', closePopup);
+
+function submitForm(event) {
+  event.preventDefault();
+  name.textContent = nameField.value;
+  description.textContent = descriptionField.value;
+  closePopup();
+}
+form.addEventListener('submit', submitForm);
