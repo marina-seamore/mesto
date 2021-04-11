17072e89f2de39eed6496c2c37b1d400bd19f5b5
Marina Abovyan <marrina.abovyan@gmail.com>
2021-04-11 10:53:52 +0545

    refactor: contsructor&public function changed

--- scripts/Card.js
+++ scripts/Card.js
@@ -1,65 +1,44 @@
 export class Card {
 
-    constructor(image, text, container) {
+    constructor(image, text, cardSelector, handleCardClick) {
         this._image = image;
         this._text = text;
         this._isLiked = false;
-        this.createCard(container);
+        this._cardSelector = cardSelector;
+        this._handleCardClick = handleCardClick;
+    }
+
+    _getTemplate = () => {
+        const cardTemplate = document
+        .querySelector(this._cardSelector)
+        .content
+        .querySelector('.element')
+        .cloneNode(true);
+        return cardTemplate;
     }
 
     _likeHandler = () => {
-        this._isLiked = !this._isLiked
-        if (this._isLiked) {
-            this._card.querySelector('.element__like-button').classList.add('element__like-button_active');
-        } else {
-            this._card.querySelector('.element__like-button').classList.remove('element__like-button_active');
-        }
+        this._card.querySelector('.element__like-button').classList.toggle('element__like-button_active');
     }
 
     _remove = () => {
         this._card.remove();
     }
 
-    _openPopupFullScreen = (event) => {
-        const popupFullPhoto = document.querySelector('.popup_full-photo');
-        popupFullPhoto.querySelector('.full-photo__image').src = event.target.closest('.element__photo').src;
-        popupFullPhoto.querySelector('.full-photo__place').textContent = event.target.closest('.element').textContent;
-        popupFullPhoto.classList.add('popup_opened');
-    }
-
-    _closePopupFullScreen = () => {
-        const popupFullPhoto = document.querySelector('.popup_full-photo');
-        popupFullPhoto.classList.remove('popup_opened');
-    }
-
-    _closeFullScreenPopupOverlay = (event) => {
-        if (event.target.classList.contains('popup')) {
-            this._closePopupFullScreen();
-        }
-      }
-            
-      _closeFullScreenPopupEsc = (event) => {
-        if (event.key === 'Escape') {
-            this._closePopupFullScreen();
-        }
-      }
-
     _setEventListeners = () => {
         this._card.querySelector('.element__delete-button').addEventListener('click', this._remove);
         this._card.querySelector('.element__like-button').addEventListener('click', this._likeHandler);
-        this._card.querySelector('.element__photo').addEventListener('click', this._openPopupFullScreen);        
-        document.querySelector('.popup__close-button-full-photo').addEventListener('click', this._closePopupFullScreen);
-        document.addEventListener('click', this._closeFullScreenPopupOverlay);
-        document.addEventListener('keydown', this._closeFullScreenPopupEsc)
+        this._card.querySelector('.element__photo').addEventListener('click', () => {
+            this._handleCardClick(this._text, this._image)
+        });
     }
 
-    static template = document.querySelector('.element-template').content;
-
-    createCard = (container) => {
-        this._card = Card.template.cloneNode(true).children[0];
+    createCard = () => {
+        this._card = this._getTemplate();
         this._card.querySelector('.element__photo').src = this._image;
+        this._card.querySelector('.element__photo').alt = this._text
         this._card.querySelector('.element__name').textContent = this._text;
         this._setEventListeners();
-        container.prepend(this._card);
+        return this._card;
     }
 }
