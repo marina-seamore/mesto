69bb982ca236e0de0d27723d43b0bf9ca1b7698d
Marina Abovyan <marrina.abovyan@gmail.com>
2021-04-26 12:06:39 +0300

    relocation: initialElements added to utils

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import PopupWithForm from '../components/PopupWithForm.js';
 import PopupWithImage from '../components/PopupWithImage.js';
 import Section from '../components/Section.js';
 import UserInfo from '../components/UserInfo.js';
-import initialElements from '../components/initialElements.js'
+import initialElements from '../utils/initialElements.js'
 
 const container = document.querySelector('.elements');
 const nameField = document.querySelector('.popup__field_type_name');
