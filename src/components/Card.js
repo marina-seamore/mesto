f5f82570ea9d641b26fb419b33cfe17ffdd14d1d
Marina Abovyan <marrina.abovyan@gmail.com>
2021-04-26 14:06:30 +0300

    refactor: selectors set separtely

--- src/components/Card.js
+++ src/components/Card.js
@@ -28,7 +28,8 @@ export default class Card {
     }
 
     _setEventListeners () {
-        this._card.querySelector('.element__delete-button').addEventListener('click', this._remove);
+        this._deleteBtn = this._card.querySelector('.element__delete-button');
+        this._deleteBtn.addEventListener('click', this._remove);
         this._card.querySelector('.element__like-button').addEventListener('click', this._likeHandler);
         this._card.querySelector('.element__photo').addEventListener('click', () => {
             this._handleCardClick(this._text, this._image)
@@ -37,8 +38,9 @@ export default class Card {
 
     createCard () {
         this._card = this._getTemplate();
-        this._card.querySelector('.element__photo').src = this._image;
-        this._card.querySelector('.element__photo').alt = this._text
+        this._cardPhoto = this._card.querySelector('.element__photo');
+        this._cardPhoto.src = this._image;
+        this._cardPhoto.alt = this._text
         this._card.querySelector('.element__name').textContent = this._text;
         this._setEventListeners();
         return this._card;
